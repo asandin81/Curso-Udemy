94de910738addaf4b8ad408758e36cfe2e45b34c
Angel <angel@sandin.cat>
2018-09-01 15:22:26 +0200

    Initial commit

---
+++ Assets/Moneda/Moneda.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moneda : MonoBehaviour {
+
+
+    public static int contador = 0;
+	// Use this for initialization
+	void Start () {
+        Moneda.contador++;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Algo choco con la moneda.");
+        Moneda.contador--;
+        if (Moneda.contador == 0)
+        {
+            Debug.Log("El juego ha termindado");
+            GameObject gameManager = GameObject.Find("GameManager");
+            Destroy(gameManager);
+
+            GameObject[] fireworksSystem = GameObject.FindGameObjectsWithTag("fireworks");
+            foreach (GameObject fireworks in fireworksSystem)
+            {
+                fireworks.GetComponent<ParticleSystem>().Play();
+            }
+        }
+    }
+}
---
+++ Assets/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour {
+
+    public float maxTime = 60f;
+    private float countDown = 0f;
+	// Use this for initialization
+	void Start () {
+        countDown = maxTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        countDown = countDown - Time.deltaTime;
+        Debug.Log("Cuenta atras : " + countDown);
+        if(countDown <= 0){
+            Debug.Log("You Lose");
+
+            Moneda.contador = 0;
+            SceneManager.LoadScene("SampleScene");
+        }
+	}
+}
